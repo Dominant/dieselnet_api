11c550171aaa182fd4e1b050f7b0bfdc6d90f4b8
Masrur <masrur.mirboboev@move24.com>
2018-03-09 11:26:03 +0100

    extract rabbit mq credentials to config

--- src/Infrastructure/Config/Config.php
+++ src/Infrastructure/Config/Config.php
@@ -4,5 +4,30 @@ namespace Dieselnet\Infrastructure\Config;
 
 class Config
 {
+    /**
+     * @var mixed
+     */
+    private $data;
 
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param string $key
+     * @param null $default
+     * @return mixed|null
+     */
+    public function get(string $key, $default = null)
+    {
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+
+        return $default;
+    }
 }
--- src/definitions.php
+++ src/definitions.php
@@ -3,6 +3,7 @@
 use Dieselnet\Application\CommandMapper;
 use Dieselnet\Domain\Authorization\Token;
 use Dieselnet\Domain\User;
+use Dieselnet\Infrastructure\Config\Config;
 use Dieselnet\Infrastructure\Events\SymfonyEventDispatcherAdapter;
 use Dieselnet\Infrastructure\Persistance;
 use Dieselnet\Application;
@@ -20,7 +21,21 @@ return [
         'Dieselnet\\Application\\Handlers\\'
     ),
 
-    AMQPStreamConnection::class => DI\create(AMQPStreamConnection::class)->constructor(
-        'localhost', 5672, 'guest', 'guest'
-    )
+    Config::class => DI\factory(function() {
+        $file = PROJECT_ROOT . '/src/config.yaml';
+        $data = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($file));
+
+        return new Dieselnet\Infrastructure\Config\Config($data);
+    }),
+
+    AMQPStreamConnection::class => DI\factory(function (Config $config) {
+        $amqpConfig = $config->get('rabbitmq');
+
+        return new AMQPStreamConnection(
+            $amqpConfig['host'],
+            $amqpConfig['port'],
+            $amqpConfig['user'],
+            $amqpConfig['pass']
+        );
+    })
 ];
