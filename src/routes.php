1f15f20407fdf46ac189191c71391dada5fb6219
Dominant <m.mirboboev@gmail.com>
2018-03-31 14:39:46 +0200

    add cors preflight request response headers

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ServerRequestInterface;
 $app->get('/', WebActions\Home\Home::class)->setName('home');
 $app->post('/signup', WebActions\User\Signup::class)->setName('signup');
 $app->post('/verify', WebActions\User\VerifyCode::class)->setName('verify-code');
-$app->options('/', function(ServerRequestInterface $request, ResponseInterface $response) {
+$app->options('/[{path:.*}]', function(ServerRequestInterface $request, ResponseInterface $response) {
     $response = $response->withHeader('Access-Control-Allow-Headers', 'X-Dieselnet-token, Content-Type');
     return $response->withStatus(200);
 })->setName('options-check');
